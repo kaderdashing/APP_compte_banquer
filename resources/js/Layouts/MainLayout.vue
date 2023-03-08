cf535f7be7d13080942abb70f51bfb7214c69b1c
kaderdashing <92338093+kaderdashing@users.noreply.github.com>
2023-03-08 11:51:11 +0100

    UpdateMainLayout
    j'ai réundonter la nav

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,24 +1,34 @@
 <template>
   <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
     <div class="container mx-auto">
-      <nav class="p-4 flex items-center justify-between">
-        <div class="text-lg font-medium">
-          <Link :href="route('listing.index')">Listings</Link>
-        </div>
-        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
-          <Link :href="route('listing.index')">LaraZillow</Link>
-        </div>
-        <div v-if="user" class="flex items-center gap-4">
-          <Link class="text-sm text-gray-500" :href="route('realtor.listing.index')">{{ user.name }}</Link>
-          <Link :href="route('realtor.listing.create')" class="btn-primary">+ New Listing</Link>
-          <div>
-            <Link :href="route('logout')" method="delete" as="button">Logout</Link>
-          </div>
-        </div>
-        <div v-else class="flex items-center gap-2">
-          <Link :href="route('user-account.create')">Register</Link>
-          <Link :href="route('login')">Sign-In</Link>
-        </div>
+      <nav class="p-4 flex flex-col sm:flex-no-wrap  sm:justify-between   justify-around">
+        <div class="flex justify-around">
+  <Link :href="route('listing.index')" class="">LOGO</Link>
+
+  <Link :href="route('listing.index')" class="text-xl text-indigo-600 dark:text-indigo-300 font-bold ">NOTIFICATION</Link>
+  <div v-if="user" class="items-center">
+    <Link :href="route('logout')" method="delete" as="button" class="ml-auto">Logout</Link>
+  </div>
+</div>
+
+
+
+
+
+
+  <div v-if="user" class="flex flex-row items-center justify-around gap-4 mt-5" >
+  <Link class="text-sm text-gray-500" :href="route('realtor.listing.index')"> statistiques </Link>
+  <Link :href="route('realtor.listing.create')" class="btn-primary">dépenses</Link>
+  <div>
+    <Link :href="route('logout')" method="delete" as="button">portefeuille</Link>
+  </div>
+</div>
+
+    <div v-else class="flex items-center justify-around mt-5">
+  <Link :href="route('user-account.create')" class="ml--10">Register</Link>
+  <Link :href="route('login')" class="mr-10">Sign-In</Link>
+</div>
+
       </nav>
     </div>
   </header>
@@ -30,6 +40,7 @@
     <slot>Default</slot>
   </main>
 </template>
+
 
 <script setup>
 import { computed } from 'vue'
